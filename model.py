109f6219823c8c8cf9072874d44adf6fd7bef861
Edward Dong <109421190+HzEdward@users.noreply.github.com>
2024-04-27 15:40:23 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,21 +9,20 @@ from PIL import Image
 from dataloader import *
 
 '''
-这是模型的主入口，包含以下功能：
-    1. initialize_model: 初始化模型、标准和优化器
-    2. train_model: 训练模型
-    3. valid_model: 验证模型
-    4. resume_training: 从检查点恢复训练
-    5. test_model: 在测试集上测试模型
-    创建检查点(create_checkpoint): 为模型创建检查点
+This is the main entry point for the model and contains the following functions:
+    1. initialize_model: initialise the model, criteria and optimiser
+    2. train_model: Train the model
+    3. valid_model: Validate the model
+    4. resume_training: resume training from checkpoints
+    5. test_model: Tests the model on a test set.
+    create_checkpoint: Creates a checkpoint for the model.
 
-note: 
-* the dataloader is defined in dataloader.py。在本文件中Dataset读取被注释了，实际上这是一个可以工作的例子。
-* __getitem__中一定要用0和1来作为返回值,否则不符合内部操作规定
+note: * the dataloader is defined. 
+* the dataloader is defined in dataloader.py. Dataset reading is commented out in this file, this is actually a working example.
+* __getitem__ must use 0 and 1 as return values, otherwise it does not comply with the internal operation rules.
 
-在运行程序时注意：
-* 为了使用GPU，需要将模型和数据转移到GPU上，设置GPU.device("cuda:0"), 否则运行效果会比较慢
-
+Note when running the programme:
+* In order to use the GPU, you need to transfer the model and data to the GPU, set GPU.device("cuda:0"), otherwise the running effect will be slower
 
 '''
 ''' the same dataloader as in dataloader.py
@@ -91,24 +90,18 @@ note:
 class SingleInputResNet(nn.Module):
     def __init__(self):
         super(SingleInputResNet, self).__init__()
-        # 加载预训练的 ResNet-50 模型
         self.resnet = models.resnet50(pretrained=True)
         
-        # 冻结模型的所有参数
         for param in self.resnet.parameters():
             param.requires_grad = False
         
-        # 修改 ResNet-50 的输入通道数
         self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
-        # 获取 ResNet-50 的全连接层输入特征数
         num_ftrs = self.resnet.fc.in_features
         
-        # 定义新的全连接层，输出维度为 2，即两个类别
         self.fc = nn.Linear(num_ftrs, 2)  # 输入特征数为 num_ftrs，输出维度为 2
 
     def forward(self, input):
-        # 传递输入图像到 ResNet-50 模型，直到最后一个卷积层的输出
         x = self.resnet.conv1(input)
         x = self.resnet.bn1(x)
         x = self.resnet.relu(x)
@@ -119,11 +112,9 @@ class SingleInputResNet(nn.Module):
         x = self.resnet.layer3(x)
         x = self.resnet.layer4(x)
         
-        # 应用全局平均池化
         x = self.resnet.avgpool(x)
-        x = torch.flatten(x, 1)  # 展平特征向量
+        x = torch.flatten(x, 1)  
         
-        # 使用新的全连接层进行分类
         output = self.fc(x)
         return output
 
